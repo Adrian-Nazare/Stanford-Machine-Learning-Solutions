8042b5dc1b44f47934c7c28fcd1a89f6d650367a
Adrian Nazare <adrian.nazare3@gmail.com>
2020-03-27 14:04:17 +0200

    Add files via upload

---
+++ ex8/selectThresholdTesting.m
@@ -0,0 +1,60 @@
+function [bestEpsilon bestF1 Fvector] = selectThreshold(yval, pval)
+%SELECTTHRESHOLD Find the best threshold (epsilon) to use for selecting
+%outliers
+%   [bestEpsilon bestF1] = SELECTTHRESHOLD(yval, pval) finds the best
+%   threshold to use for selecting outliers based on the results from a
+%   validation set (pval) and the ground truth (yval).
+%
+
+bestEpsilon = 0;
+bestF1 = 0;
+F1 = 0;
+
+Fvector = [];
+tpvector = [];
+fpvector = [];
+fnvector = [];
+
+stepsize = (max(pval) - min(pval)) / 1000;
+for epsilon = min(pval):stepsize:max(pval)
+    
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Compute the F1 score of choosing epsilon as the
+    %               threshold and place the value in F1. The code at the
+    %               end of the loop will compare the F1 score for this
+    %               choice of epsilon and set it to be the best epsilon if
+    %               it is better than the current choice of epsilon.
+    %               
+    % Note: You can use predictions = (pval < epsilon) to get a binary vector
+    %       of 0's and 1's of the outlier predictions
+    
+cvPredictions = pval < epsilon;
+tp = sum(yval & cvPredictions);
+tpvector = [tpvector tp];
+fp = sum(~yval & cvPredictions);
+fpvector = [fpvector fp];
+fn = sum(yval & ~cvPredictions);
+fnvector = [fnvector fn];
+
+if tp == 0
+    precision = 1;
+else precision = tp / (tp + fp);
+end
+
+if tp == 0
+    recall = 0;
+else recall = tp/( tp + fn);
+end
+
+F1 = (precision * recall) / (precision + recall);
+Fvector = [Fvector F1];
+
+    % =============================================================
+
+    if F1 > bestF1
+       bestF1 = F1;
+       bestEpsilon = epsilon;
+    end
+end
+
+end
